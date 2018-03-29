fb0fd7e2124f3377fcafd732d5579836cb04d657
SoSeductive <den.aka.dizel@gmail.com>
2018-03-29 18:37:02 +0300

    Test app

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,13 @@
-// You can import your modules
-// const index = require('../index')
+const octokit = require('@octokit/rest')()
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
-})
+function getRepo(org, repo) {
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
+  octokit.repos.getForOrg({
+    org: org,
+    type: 'public'
+  }).then(({data}) = > {
+    console.log(data);
+  }
+)
+
+}
