627f70bfa6539372898def1ac49c8c9fb6a1edbf
SoSeductive <den.aka.dizel@gmail.com>
2018-03-30 14:23:38 +0300

    Test app

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,3 @@
-jQuery(document).ready(function(){
-console.log('123');
+jQuery(document).ready(function () {
+  console.log('123');
 })
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,6 @@
 // You can import your modules
 // const index = require('../index')
 
-const getRepo = (url) => {
-  return fetch(url)
-      .then((res) => res.json());
-};
-
 test('that we can run tests', () => {
   // your real tests go here
   expect(1 + 2 + 3).toBe(6)
